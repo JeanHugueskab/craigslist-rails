b41538e099bda8040b628508e3eab67c3f937945
John Winslow & ArlenePerez <pair+john-winslow.techforchange@devbootcamp.com>
2016-12-13 15:48:57 -0800

    Createss new article form and show page

--- craigslist/app/controllers/articles_controller.rb
+++ craigslist/app/controllers/articles_controller.rb
@@ -1,5 +1,28 @@
 class ArticlesController < ApplicationController
-  def 
+  def new
+    @category = Category.find(params[:category_id])
+    @article = @category.articles.new
   end
 
+  def create
+    @category = Category.find(params[:category_id])
+    @article = @category.articles.new(article_params)
+    @article.user_id = session[:user_id]
+    if @article.save
+      redirect_to category_path(@category)
+    else
+      @errors = @article.errors.full_messages
+      render 'new'
+    end
+  end
+
+  def show
+    @category = Category.find(params[:category_id])
+    @article = Article.find(params[:id])
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
